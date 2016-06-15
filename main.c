4ab23f4472126f2925cc6b523ce0afba9bbee4e5
dommich <dominik.mich@autonomik.pl>
2016-06-15 16:25:13 +0200

    pętla while(1)

--- main.c
+++ main.c
@@ -5,4 +5,16 @@
  *      Author: Dominik
  */
 
+#ifndef F_CPU
+#define F_CPU 16000000UL // 16 MHz clock speed
+#endif
 
+#include <avr/io.h>
+
+int main(void)
+{
+  while(1) //infinite loop
+  {
+
+  }
+}
